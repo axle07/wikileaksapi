84d404c9b53fba724db46323e72fe4eec541ff19
Alex <meetalexjohnson@gmail.com>
2016-08-28 08:10:15 -0700

    to, from, subject return only wl ids

--- routes/clinton-emails.js
+++ routes/clinton-emails.js
@@ -23,7 +23,7 @@ module.exports = function (app, passport) {
     *      GET: finds emails by sender
     */
     app.get("/clinton-emails/from/:key/:from", isValidKey, function(req, res) {
-        db.emails.collection(config.emailCollection).find({from: req.params.from}).toArray( function(err, doc) {
+        db.emails.collection(config.emailCollection).find({from: req.params.from}, {_id: 0, id: 1}).toArray( function(err, doc) {
             if (err) {
                 handleError(res, err.message, "Failed to get email");
             } else {
@@ -36,7 +36,7 @@ module.exports = function (app, passport) {
      *      GET: finds emails by recipient
      */
     app.get("/clinton-emails/to/:key/:to", isValidKey, function(req, res) {
-        db.emails.collection(config.emailCollection).find({to: req.params.to}).toArray( function(err, doc) {
+        db.emails.collection(config.emailCollection).find({to: req.params.to}, {_id: 0, id: 1}).toArray( function(err, doc) {
             if (err) {
                 handleError(res, err.message, "Failed to get email");
             } else {
@@ -49,7 +49,7 @@ module.exports = function (app, passport) {
      *      GET: finds emails by subject
      */
     app.get("/clinton-emails/subject/:key/:subject", isValidKey, function(req, res) {
-        db.emails.collection(config.emailCollection).find({subject: req.params.subject}).toArray( function(err, doc) {
+        db.emails.collection(config.emailCollection).find({subject: req.params.subject}, {_id: 0, id: 1}).toArray( function(err, doc) {
             if (err) {
                 handleError(res, err.message, "Failed to get email");
             } else {
